54117b9c3bfe13fb667afd26efb2451e04fc8c07
Axel Aubry <Axel Aubry>
2016-03-08 11:03:15 +0100

    makefile ok

---
+++ main.h
@@ -0,0 +1,23 @@
+// Projet: Oxymetre
+// Auteurs: Axel AUBRY - Pierre PICARD
+// Date de creation: 27/02/2016
+// Role: fonction principale du projet
+
+#ifndef MAIN_H
+#define MAIN_H
+// Includes.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+struct absorp{
+    float ACR;
+    float DCR;
+    float ACIR;
+    float DCIR;
+};
+
+
+// Sources codes
+#endif
