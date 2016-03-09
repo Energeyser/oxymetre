b4c8e0fcd01f770014d0c4c25fc40f79337768b3
pierre Picard <pierre Picard>
2016-03-09 11:45:59 +0100

    egh

--- iir.c
+++ iir.c
@@ -5,21 +5,4 @@
 
 #include "iir.h"
 
-//-------------------------------------------------------------------------
-//
-//
-// retourne une structure
-
-/*struct absorp iir(struct absorp myAbsorp, struct absorp* mem_iir)
-{
-    int valeurACR;
-    int valeurACIR;
-    valeurACR = *mem_iir.ACR;
-    *mem_iir.ACR = myAbsorp.ACR;
-    valeurACIR = mem_iir.ACIR;
-    mem_iir.ACIR = myAbsorp.ACIR;
-    myAbsorp.ACR = mem_iir.ACR - valeurACR + 0.992 * myAbsorp.ACR;
-    //printf("IIR : tab filtre iir ACr : %f\n", myAbsorp.ACR);
-    myAbsorp.ACIR = mem_iir.ACIR - valeurACIR + 0.992 * myAbsorp.ACIR;
-    return myAbsorp;
-}*/
+//----------------------------------------------------------------------
