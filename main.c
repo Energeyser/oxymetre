94cda45650a8e104e3aef71fb59eee38e9434548
pierre Picard <pierre Picard>
2016-03-09 11:46:15 +0100

    rtb

--- main.c
+++ main.c
@@ -5,19 +5,3 @@
 
 #include "main.h"
 
-int main()
-{
-    struct absorp myAbsorp;
-    struct oxy myOxy;
-    bool etat = 1;
-    int mem_fir;
-    struct absorp mem_iir;
-    int coeffFIR[51];
-    //iir(myAbsorp, mem_iir);
-    FILE* descr = NULL;
-
-    descr = initDescr();
-    myAbsorp = fir(myAbsorp, mem_fir);
-
-    return 0;
-}
